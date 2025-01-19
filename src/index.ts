e7d71d9aae64df98ddb64e7bf5ae2d5f965c022f
Genius Chinembiri <geniuschinembiri@gmail.com>
2025-01-19 12:15:46 +0200

    Export CustomError

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import { createApp } from "./app";
 import { createLogger } from "./logs";
 import { route } from "./route";
 export type { Logger } from "./logs";
-export type { CustomError } from "./custom-error";
-import { isCustomError } from "./custom-error";
+import { isCustomError, CustomError } from "./custom-error";
 
-export { route, createApp, createLogger, isCustomError };
+export { route, createApp, createLogger, isCustomError, CustomError };
 
 export default {
   route,
   createApp,
   createLogger,
   isCustomError,
+  CustomError,
 };
