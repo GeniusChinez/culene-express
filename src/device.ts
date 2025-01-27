8f3dc7563ec5c6cad6140c2cfe1e44116ddc16aa
Genius Chinembiri <geniuschinembiri@gmail.com>
2025-01-27 06:39:52 +0200

    Add more details to context device

--- src/device.ts
+++ src/device.ts
@@ -3,6 +3,27 @@ import { Details } from "express-useragent";
 export function getDeviceId(useragent: Details) {
   const browser = useragent?.browser;
   const os = useragent?.os;
+  const type = (() => {
+    if (!useragent) {
+      return "none";
+    }
+    if (useragent.isBot) {
+      return "bot";
+    }
+    if (useragent.isTablet) {
+      return "tablet";
+    }
+    if (useragent.isDesktop) {
+      return "desktop";
+    }
+    if (useragent.isMobile) {
+      return "mobile";
+    }
+    if (useragent.isSmartTV) {
+      return "smart-tv";
+    }
+    return "other";
+  })();
 
   const name = JSON.stringify({
     browser,
@@ -13,5 +34,7 @@ export function getDeviceId(useragent: Details) {
     browser,
     os,
     name,
-  };
+    type,
+    other: useragent,
+  } as const;
 }
