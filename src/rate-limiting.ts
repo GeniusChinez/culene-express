9f67761d4a4be46cbc7f740e86a5ec29e4e58ee2
Genius Chinembiri <geniuschinembiri@gmail.com>
2025-01-27 11:01:30 +0200

    Use custom handler for 429 responses

--- src/rate-limiting.ts
+++ src/rate-limiting.ts
@@ -25,6 +25,15 @@ export function getRateLimitingOptions(config: RateLimitingConfig) {
       max: config.requests, // Limit each IP to these requests per `window`
       standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
       legacyHeaders: false,
+      handler: (_req, res, _next, options) => {
+        res.status(429).json({
+          message:
+            "You have exceeded the number of allowed requests. Please wait and try again later.",
+          rateLimitResetTime: new Date(
+            Date.now() + options.windowMs,
+          ).toISOString(), // Optional: the reset time
+        });
+      },
     } as Partial<RateLimitingOptions>;
   }
   return config as RateLimitingOptions;
